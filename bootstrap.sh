5dc16bb35ade7d54f163354efa674651ea0ce8b1
Jeong Woo Kim <50034678+JungWKim@users.noreply.github.com>
2023-05-05 17:45:03 +0900

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -18,15 +18,6 @@ sudo systemctl disable ufw
 sudo apt update
 sudo apt install -y net-tools nfs-common whois
 
-# network configuration
-sudo modprobe overlay \
-    && sudo modprobe br_netfilter
-
-cat <<EOF | sudo tee -a /etc/modules-load.d/containerd.conf
-overlay
-br_netfilter
-EOF
-
 cat <<EOF | sudo tee -a /etc/sysctl.d/99-kubernetes-cri.conf
 net.bridge.bridge-nf-call-iptables  = 1
 net.ipv4.ip_forward                 = 1
